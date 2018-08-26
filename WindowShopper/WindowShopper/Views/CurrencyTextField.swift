d4f79756254f20d57a9861618f49c7b4b33c4f3f
Julia Nikitina <julianikitina.ios@gmail.com>
2018-08-27 01:12:11 +0300

    Add currency labels

--- WindowShopper/WindowShopper/Views/CurrencyTextField.swift
+++ WindowShopper/WindowShopper/Views/CurrencyTextField.swift
@@ -12,6 +12,27 @@ final class CurrencyTextField: UITextField {
         setUpViews()
     }
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        
+        let lblFrame = CGRect(x: 15, y: ((frame.size.height / 2) - size / 2), width: size, height: size)
+        
+        let currencyLbl = UILabel(frame: lblFrame)
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 0.4780340325)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
+    }
+    
     private func setUpViews() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5
