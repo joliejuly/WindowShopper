525e1730bbba1f6eeb06352caa3115b1eb579f1b
Julia Nikitina <julianikitina.ios@gmail.com>
2018-08-27 00:07:44 +0300

    Add monthly wage logic to the model

--- WindowShopper/WindowShopper/Controllers/MainScreen.swift
+++ WindowShopper/WindowShopper/Controllers/MainScreen.swift
@@ -20,6 +20,39 @@ class MainScreen: UIViewController {
         setUpCalculateView()
     }
     
+    @objc private func calculateBtnTapped() {
+        guard let wageText = wageTxt.text,
+            let priceText = priceTxt.text else { return }
+        
+        guard let wage = Double(wageText),
+            let price = Double(priceText) else { return }
+        
+        view.endEditing(true)
+        
+        quantityOfHoursLbl.isHidden = false
+        hoursLbl.isHidden = false
+        
+        let resultHours = String(Wage.getHours(forWage: wage, andPrice: price))
+        
+        quantityOfHoursLbl.text = resultHours
+        
+        resultHours.hasSuffix("1") ? (hoursLbl.text = "hour") : (hoursLbl.text = "hours")
+    }
+    
+    
+    @IBAction func clearBtnTapped(_ sender: UIButton) {
+        quantityOfHoursLbl.isHidden = true
+        hoursLbl.isHidden = true
+        
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
+    private func setUpViews() {
+        quantityOfHoursLbl.isHidden = true
+        hoursLbl.isHidden = true
+    }
+    
     private func setUpCalculateView() {
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calcBtn.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
@@ -30,10 +63,15 @@ class MainScreen: UIViewController {
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
     }
-
-    @objc private func calculateBtnTapped() {
-        print("Hey there")
-    }
-
+    
 }
+
+
+
+
+
+
+
+
+
 
--- WindowShopper/WindowShopper/Models/Wage.swift
+++ WindowShopper/WindowShopper/Models/Wage.swift
@@ -4,7 +4,10 @@ import Foundation
 final class Wage {
     
     class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
-        return Int(ceil(price / wage))
+        
+        let workingHoursInMonth: Double = 8 * 5 * 4
+        let hourlyWage = wage / workingHoursInMonth
+        return Int(ceil(price / hourlyWage))
     }
     
 }
