b0213b9e1d8a10d38226c9d041399a1e9b8be0e3
Julia Nikitina <julianikitina.ios@gmail.com>
2018-08-26 22:49:58 +0300

    Add outlets to Main screen

--- WindowShopper/WindowShopper/Controllers/MainScreen.swift
+++ WindowShopper/WindowShopper/Controllers/MainScreen.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class MainScreen: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    @IBOutlet weak var quantityOfHoursLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setUpCalculateView() {
+        
     }
 
 
