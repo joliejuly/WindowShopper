ec20b59c195751b35fb3fbf1bc3a5a73ef7bfa5c
Julia Nikitina <julianikitina.ios@gmail.com>
2018-08-26 22:58:42 +0300

    Add accessory view button to the keyboards

--- WindowShopper/WindowShopper/Controllers/MainScreen.swift
+++ WindowShopper/WindowShopper/Controllers/MainScreen.swift
@@ -15,16 +15,25 @@ class MainScreen: UIViewController {
     @IBOutlet weak var quantityOfHoursLbl: UILabel!
     @IBOutlet weak var hoursLbl: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUpCalculateView()
     }
     
     private func setUpCalculateView() {
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(calculateBtnTapped), for: .touchUpInside)
         
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
+    @objc private func calculateBtnTapped() {
+        print("Hey there")
+    }
 
 }
 
